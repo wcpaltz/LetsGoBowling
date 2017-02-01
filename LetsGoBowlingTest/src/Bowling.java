66b9f3c53728c213b45aa20289f484a470b3cb4e
DESKTOP-7DHAQP7\Julian-Laptop <bleckin2@uwm.edu>
2017-02-01 16:20:37 -0600

    Added interface

--- LetsGoBowlingTest/src/Bowling.java
+++ LetsGoBowlingTest/src/Bowling.java
@@ -1,4 +1,18 @@
 
 public class Bowling {
-	int i = 42;
+	public int getScore(){
+		return -1;
+	}
+	
+	public void addThrow(int first, int second){
+		
+	}
+	
+	public void addThrowStrike(){
+		
+	}
+	
+	public int getCurrentFrameNum(){
+		return -1;
+	}
 }
