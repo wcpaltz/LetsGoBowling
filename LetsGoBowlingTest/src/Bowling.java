04daeaf65365a83e11b6e9daa772791fcff124f5
DESKTOP-7DHAQP7\Julian-Laptop <bleckin2@uwm.edu>
2017-02-01 16:46:07 -0600

    Laid Framework for Bowling class

--- LetsGoBowlingTest/src/Bowling.java
+++ LetsGoBowlingTest/src/Bowling.java
@@ -1,18 +1,52 @@
 
 public class Bowling {
-	public int getScore(){
+	
+	private Frame[] scoresheet;
+	private int frameNum;
+	
+	private class Frame{
+		
+		int t1, t2;
+		
+		private Frame(int t1, int t2){
+			this.t1 = t1;
+			this.t2 = t2;
+		}
+		
+		public boolean isStrike(){
+			return t1 == 10;
+		}
+		
+		public boolean isSpare(){
+			return !isStrike() && (t1 + t2 == 10);
+		}
+		
+		public int getRawScore(){
+			return t1 + t2;
+		}
+		
+	}
+	
+	
+	public Bowling(){
+		scoresheet = new Frame[10];
+		frameNum = 0;
+	}
+	
+	public int getFrameScore(int frameNum){
+		return -1;
+	}
+	
+	public int getTotalScore(){
 		return -1;
 	}
 	
 	public void addThrow(int first, int second){
-		
-	}
-	
-	public void addThrowStrike(){
-		
+		scoresheet[frameNum] = new Frame(first, second);
 	}
 	
 	public int getCurrentFrameNum(){
-		return -1;
+		return frameNum + 1;
 	}
+	
 }
