3f4c83154c66f2c9573d6ca8118f3cd12af153d3
DESKTOP-7DHAQP7\Julian-Laptop <bleckin2@uwm.edu>
2017-02-02 14:08:36 -0600

    Finished all the methods - please check my work!

--- LetsGoBowlingTest/src/Bowling.java
+++ LetsGoBowlingTest/src/Bowling.java
@@ -1,10 +1,10 @@
 
 public class Bowling {
 	
-	private Frame[] scoresheet;
-	private int frameNum;
+	private Frame[] scoresheet; //array that holds all the frames of a bowling game
+	private int frameNum; //the number of the frame the player is currently on
 	
-	private class Frame{
+	private class Frame{ //Frame of a bowling game
 		
 		int t1, t2, frameScore;
 		
@@ -28,30 +28,47 @@ public class Bowling {
 	}
 	
 	
-	public Bowling(){
+	public Bowling(){ //constructor
 		scoresheet = new Frame[10];
 		frameNum = 0;
 	}
 	
-	public int getFrameScore(int frameNum){ 
-		return -1;
+	public int getFrameScore(int frame){ //returns the score in that frame, at the current time
+		return scoresheet[frame].frameScore;
 	}
 	
-	public int getTotalScore(){
+	public int getTotalScore(){ //returns current total score
 		
 		int finalScore = 0;
 		
-		if (scoresheet[0] == null) return finalScore;
+		if (scoresheet[0] == null) return finalScore; //prevents case that no bowling has yet occurred
 		
-		for(int i = frameNum; i > -1; i --){
+		for(int i = 0; i < frameNum + 1; i ++){
 			finalScore += getFrameScore(i);
 		}
 		
 		return finalScore;
 	}
 	
-	public void addThrow(int first, int second){ //adds new Frame & increments
+	public void addThrow(int first, int second){ //adds new Frame, updates frameScores & increments
+		
+		if(first + second > 10 || first > 10 || second > 10) throw new IllegalArgumentException("Invalid bowling entry.");
+		
 		scoresheet[frameNum] = new Frame(first, second);
+		
+		for(int i = 0; i < frameNum + 1; i++){
+			
+			if(scoresheet[i].isStrike() && frameNum >= i + 2)
+				scoresheet[i].frameScore = scoresheet[i].getRawScore() + scoresheet[i+1].getRawScore()
+					+ scoresheet[i+2].getRawScore();
+			
+			else if(scoresheet[i].isSpare() && frameNum >= i + 1)
+				scoresheet[i].frameScore = scoresheet[i].getRawScore() + scoresheet[i+1].getRawScore();
+			else
+				scoresheet[i].frameScore = scoresheet[i].getRawScore();
+			
+		}
+		
 		frameNum++;
 	}
 	
